d16f0ad2170ab9a6c2b040208a1d29a2d8fad21c
Andrés Banda <abanda@cogcs.com>
2023-09-02 00:36:00 -0500

    Add mobile query to modify calendly width

--- src/app/[lng]/modules/ContactSection/ContactSection.tsx
+++ src/app/[lng]/modules/ContactSection/ContactSection.tsx
@@ -1,10 +1,9 @@
 'use client';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './ContactSection.module.scss';
 import globalStyles from '../../page.module.scss';
 import { InlineWidget } from 'react-calendly';
 import { PopupButton } from 'react-calendly';
-import Image from 'next/image';
 import Linkedin from '../../../../../public/linkedin.svg';
 import Github from '../../../../../public/github.svg';
 import Link from 'next/link';
@@ -16,6 +15,21 @@ const ContactSection: React.FC<IContactSectionProps> = ({
   layout = 'block'
 }) => {
   const ref = useRef(null);
+
+  const [matchMobileQuery, setMatchMobileQuery] = useState(false);
+  useEffect(() => {
+    const handleMediaQueryChange = (event: MediaQueryListEvent) => {
+      setMatchMobileQuery(event.matches);
+    };
+
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    setMatchMobileQuery(mediaQuery.matches);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
 
   return (
     <section className={globalStyles.contact} ref={ref} id="test">
@@ -68,7 +82,11 @@ const ContactSection: React.FC<IContactSectionProps> = ({
         {layout === 'block' && (
           <InlineWidget
             url="https://calendly.com/andres-banda/15min?hide_event_type_details=1&hide_gdpr_banner=1"
-            styles={{ height: '520px', minWidth: '400px', overflow: 'hidden' }}
+            styles={{
+              height: '520px',
+              minWidth: matchMobileQuery ? '300px' : '400px',
+              overflow: 'hidden'
+            }}
           />
         )}
         {layout === 'inline' && (
