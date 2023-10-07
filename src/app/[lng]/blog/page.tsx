7bce4c5c6cc469a93c559952aaa4cc41c7172e3a
Andrés Banda <andrezh_gln7@live.com>
2023-10-07 16:33:24 -0500

    Add blog home header

--- src/app/[lng]/blog/page.tsx
+++ src/app/[lng]/blog/page.tsx
@@ -1,10 +1,17 @@
 import BlogCards from '../modules/BlogCards';
-import styles from './blogHome.module.css';
+import styles from './blogHome.module.scss';
+import globalStyles from '../page.module.scss';
 
 function Blog() {
   return (
-    <section className={styles.blog}>
-      <BlogCards maxCards={100} isFeatured={true} cta="Load more" />
+    <section className={globalStyles.blogHome}>
+      <div className={globalStyles.container}>
+        <div className={styles.blogHeader}>
+          <h1>Blog</h1>
+          <p>Exploring Mobile and Web Application Security.</p>
+        </div>
+        <BlogCards maxCards={100} isFeatured={true} cta="Load more" />
+      </div>
     </section>
   );
 }
