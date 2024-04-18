0317c447f3e97a0484397e19848ca21d5b7cf2d9
Andrés Banda <andrezh_gln7@live.com>
2024-04-17 23:54:10 -0500

    Add launch project as featured project

--- src/data/projectCards.ts
+++ src/data/projectCards.ts
@@ -1,18 +1,30 @@
 import Codercat from '../../public/codercat.png';
-import Ciodesia from '../../public/ciodesia.png';
+import Launch from '../../public/launch.png';
 import Securelab from '../../public/securelab.png';
 import { IRowCards } from '../app/[lng]/components/Cards/RowCards/RowCards';
 import { IRowCard } from '../app/[lng]/components/Cards/VerticalCard/Card';
 
 export const ProjectCards: IRowCard[] = [
+  // {
+  //   id: '1',
+  //   title: 'Ciodesia',
+  //   description:
+  //     'Ciodesia is an education platform to provide training and services related to geography.',
+  //   link: 'https://ciodesia.com',
+  //   image: {
+  //     src: Ciodesia,
+  //     alt: '',
+  //     title: ''
+  //   }
+  // },
   {
     id: '1',
-    title: 'Ciodesia',
+    title: 'Launch',
     description:
-      'Ciodesia is an education platform to provide training and services related to geography.',
-    link: 'https://ciodesia.com',
+      'Launch is an AI-powered platform designed to streamline the process of testing and launching your business ideas.',
+    link: 'https://launch-black.vercel.app/',
     image: {
-      src: Ciodesia,
+      src: Launch,
       alt: '',
       title: ''
     }
