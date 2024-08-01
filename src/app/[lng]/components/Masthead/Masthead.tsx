fb85b3326f181872def7c8fc1c2efe612aa93c06
Andrés Banda <andrezh_gln7@live.com>
2024-08-01 00:11:22 -0500

    update homepage masthead: add underline and replace pattern

--- src/app/[lng]/components/Masthead/Masthead.tsx
+++ src/app/[lng]/components/Masthead/Masthead.tsx
@@ -1,5 +1,7 @@
 import styles from './Masthead.module.scss';
 import globalStyles from '../../page.module.scss';
+import Image from 'next/image';
+import Underline from '../../../../../public/underline.svg';
 
 interface IMasthead {
   title: string;
@@ -12,7 +14,8 @@ const Masthead: React.FC<IMasthead> = ({ title, description, cta }) => {
       <div className={globalStyles.container}>
         <div className={styles.masthead__inner}>
           <header className={styles.masthead__content}>
-            <h1 className={styles.masthead__title}>{title}</h1>
+            <h1 className={styles.masthead__title}>Hi, I'm Andres, Mobile and Web developer.</h1>
+            <Underline />
             <p className={styles.masthead__description}>{description}</p>
             <a className={styles.masthead__btn} href="#contact">
               {cta}
