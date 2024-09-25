db55536610d2a00269d59e979a5c8deb2f72a531
Andrés Banda <andrezh_gln7@live.com>
2024-09-25 17:22:39 -0500

    modify recent projects for localization

--- src/app/[lng]/modules/RecentProjects.tsx
+++ src/app/[lng]/modules/RecentProjects.tsx
@@ -1,15 +1,28 @@
-import { Projects } from '@/data/projectCards';
+import { ProjectCards } from '@/data/projectCards';
 import RowCards from '../components/Cards/RowCards/RowCards';
+import RowModalCards from '../components/Cards/RowModalCards/RowModalCards';
 import globalStyles from '../page.module.scss';
 
-const RecentProjects = () => {
+const RecentProjects = ({ localizedProjects }: { localizedProjects: any }) => {
+  const combinedProjects = ProjectCards.map((project, index) => {
+    const localizedProject =
+      localizedProjects && localizedProjects[index] ? localizedProjects[index] : {};
+    return {
+      ...project,
+      title: localizedProject.title || project.title,
+      description: localizedProject.description || project.description,
+      extra: {
+        ...project.extra
+      }
+    };
+  });
+
   return (
     <section
       className={globalStyles.projects}
       style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
     >
-      <RowCards {...Projects} />
-      {/* <button className={globalStyles.btn}>See more</button> */}
+      <RowModalCards cards={combinedProjects} />
     </section>
   );
 };
--- src/data/projectCards.ts
+++ src/data/projectCards.ts
@@ -1,65 +1,75 @@
 import Codercat from '../../public/codercat.png';
 import Launch from '../../public/launch.png';
-import Securelab from '../../public/securelab.png';
-import { IRowCards } from '../app/[lng]/components/Cards/RowCards/RowCards';
+import partersLogo from '../../public/recentProjects/partnersLogo.png';
+import CodercatScreen from '../../public/recentProjects/capturaCodercat.png';
+import PartnersScreen from '../../public/recentProjects/capturaPartners.png';
+import LaunchScreen from '../../public/recentProjects/capturaLaunch.png';
 import { IRowCard } from '../app/[lng]/components/Cards/VerticalCard/Card';
 
 export const ProjectCards: IRowCard[] = [
-  // {
-  //   id: '1',
-  //   title: 'Ciodesia',
-  //   description:
-  //     'Ciodesia is an education platform to provide training and services related to geography.',
-  //   link: 'https://ciodesia.com',
-  //   image: {
-  //     src: Ciodesia,
-  //     alt: '',
-  //     title: ''
-  //   }
-  // },
   {
     id: '1',
+    title: 'Partners',
+    description:
+      'Partners es una app que conecta emprendedores en etapas tempranas con socios estratégicos que complementen sus habilidades.', // Default description
+    image: {
+      src: partersLogo,
+      alt: '',
+      title: ''
+    },
+    technologies: ['javascript', 'next'],
+    extra: {
+      impact:
+        'Secure Lab has helped over 1000 students to learn cibersecurity and protect their data.',
+      screen: {
+        src: PartnersScreen,
+        alt: '',
+        title: ''
+      }
+    }
+  },
+  {
+    id: '2',
     title: 'Launch',
     description:
-      'Launch is an AI-powered platform designed to streamline the process of testing and launching your business ideas.',
-    link: 'https://launch-black.vercel.app/',
+      'Launch is an AI-powered platform designed to streamline the process of testing and launching your business ideas.', // Default description
     image: {
       src: Launch,
       alt: '',
       title: ''
     },
-    cardIcons: ['react', 'js', 'vite']
+    technologies: ['javascript', 'vite'],
+    extra: {
+      impact:
+        'Launch has helped over 1000 entrepreneurs to test their ideas and get feedback from potential customers.', // Default impact
+      link: 'https://launch-black.vercel.app/',
+      screen: {
+        src: LaunchScreen,
+        alt: '',
+        title: ''
+      }
+    }
   },
   {
-    id: '2',
+    id: '3',
     title: 'Codercat',
     description:
-      'Codercat is a web and mobile app that provides daily tips on accesibility, security and user experience.',
-    link: 'https://codercat.vercel.app',
+      'Codercat is a web and mobile app that provides daily tips on accesibility, security and user experience.', // Default description
     image: {
       src: Codercat,
       alt: '',
       title: ''
     },
-    cardIcons: ['react', 'js', 'vite']
-  },
-  {
-    id: '3',
-    title: 'Secure Lab',
-    description:
-      'Secure Lab is a mobile app designed to teach cibersecurity  with short tutorials.',
-    link: '/securelab',
-    image: {
-      src: Securelab,
-      alt: '',
-      title: ''
-    },
-    cardIcons: ['react', 'js', 'vite']
+    technologies: ['javascript', 'vite'],
+    extra: {
+      impact:
+        'Codercat has helped over 1000 developers to improve their skills and create better software.',
+      link: 'https://codercat.vercel.app',
+      screen: {
+        src: CodercatScreen,
+        alt: '',
+        title: ''
+      }
+    }
   }
 ];
-
-export const Projects: IRowCards = {
-  title: 'Recent Projects',
-  cards: ProjectCards,
-  orientation: 'vertical'
-};
