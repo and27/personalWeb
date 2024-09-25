8d87e7133ad0c6a5e65efbf2c401ea06c519b87e
Andrés Banda <andrezh_gln7@live.com>
2024-09-25 11:07:08 -0500

    feat: add row modal cards component

---
+++ src/app/[lng]/components/Cards/RowModalCards/RowModalCards.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import styles from './RowModalCards.module.scss';
+import globalStyles from '../../../page.module.scss';
+import React, { useState } from 'react';
+import { Card, IRowCard } from '../VerticalCard/Card';
+import { HorizontalCard } from '../HorizontalCard/HorizontalCard';
+import CardModal from '../../CardModal/CardModal';
+
+export interface IRowCards {
+  title?: string;
+  cards?: IRowCard[];
+  linkLabel?: string;
+  orientation?: 'horizontal' | 'vertical';
+  isFeatured?: boolean;
+  cta?: string;
+  imageOverflow?: 'hidden' | 'visible';
+}
+
+const RowModalCards: React.FC<IRowCards> = ({
+  title,
+  cards,
+  linkLabel,
+  imageOverflow,
+  orientation
+}) => {
+  const [selectedCard, setSelectedCard] = useState<IRowCard | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleCardClick = (card: IRowCard, event: React.MouseEvent) => {
+    setSelectedCard(card);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedCard(null);
+  };
+
+  return (
+    <div className={`${styles.rowCards__container}`} id={title?.toLowerCase().replace(' ', '')}>
+      <div className={globalStyles.container}>
+        {title && (
+          <h2 className={`${globalStyles.section__title} ${styles.rowCardsTitle}`}>{title}</h2>
+        )}
+        <div
+          className={`${styles.rowCards} 
+            ${orientation === 'horizontal' && styles.rowCards__horizontal}
+            ${imageOverflow === 'hidden' && styles.rowCards__noOverflow}`}
+        >
+          {cards?.map((card: any, index: number) => {
+            const CardComponent = orientation === 'horizontal' ? HorizontalCard : Card;
+            return (
+              <div
+                key={index}
+                onClick={e => handleCardClick(card, e)}
+                role="button"
+                tabIndex={0}
+                aria-label={`View details of ${card.title}`}
+                onKeyPress={e => e.key === 'Enter' && handleCardClick(card, e)}
+              >
+                <CardComponent
+                  id={`card${index.toString()}`}
+                  title={card.title}
+                  description={card.description}
+                  link={card.link}
+                  linkLabel={linkLabel}
+                  subtitle={card.subtitle}
+                  image={card.image}
+                  technologies={card.technologies}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <CardModal isOpen={isModalOpen} onClose={handleCloseModal} card={selectedCard} />
+    </div>
+  );
+};
+
+export default RowModalCards;
