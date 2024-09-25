4fd9ad55f7a9d28cf0bdba1624b6c4a9a61eaa49
Andrés Banda <andrezh_gln7@live.com>
2024-09-25 17:26:52 -0500

    modify benefits for localization

--- src/app/[lng]/components/FeaturedProjects/FeaturedProjects.tsx
+++ src/app/[lng]/components/FeaturedProjects/FeaturedProjects.tsx
@@ -2,31 +2,25 @@ import React from 'react';
 import styles from './FeaturedProjects.module.scss';
 import globalStyles from '../../page.module.scss';
 
-const FeaturedProjects = () => (
+interface IFeaturedProjectsProps {
+  title: string;
+  services: {
+    title: string;
+    description: string;
+  }[];
+}
+
+const FeaturedProjects = ({ title, services }: IFeaturedProjectsProps) => (
   <section className={globalStyles.benefits}>
     <div className={globalStyles.container}>
-      <h2 className={globalStyles.section__title}>What do I do?</h2>
+      <h2 className={globalStyles.section__title}>{title}</h2>
       <div className={styles.cards}>
-        <div className={styles.card__left}>
-          <h3 className={styles.card__title}>Frontend Development</h3>
-          <p className={styles.card__description}>
-            Build responsive websites and web apps with React, Next.js, TypeScript, and more.
-            Emphasis on security and user-centered solutions.
-          </p>
-        </div>
-        <div className={styles.card__right}>
-          <h3 className={styles.card__title}>Application Security Testing</h3>
-          <p className={styles.card__description}>
-            Ensure application protection from cyber threats with our expertise in security testing.
-          </p>
-        </div>
-        <div className={styles.card__left}>
-          <h3 className={styles.card__title}>Custom Digital Experience</h3>
-          <p className={styles.card__description}>
-            Tailored solutions for web and mobile apps, from prototyping to enhanced user
-            experiences.
-          </p>
-        </div>
+        {services?.map((service, idx) => (
+          <div key={idx} className={idx % 2 === 0 ? styles.card__left : styles.card__right}>
+            <h3 className={styles.card__title}>{service.title}</h3>
+            <p className={styles.card__description}>{service.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   </section>
