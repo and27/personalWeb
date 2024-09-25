3cc1f82ec24f6c645b93a71a47f94284ce2b656d
Andrés Banda <andrezh_gln7@live.com>
2024-09-25 17:13:39 -0500

    add localized dynamic words on masthead

--- src/app/[lng]/components/Masthead/Masthead.tsx
+++ src/app/[lng]/components/Masthead/Masthead.tsx
@@ -1,20 +1,24 @@
 import styles from './Masthead.module.scss';
 import globalStyles from '../../page.module.scss';
-import Underline from '../../../../../public/underline.svg';
+import DynamicWords from '../DynamicWords.tsx/DynamicWords';
 
 interface IMasthead {
   title: string;
   description: string;
   cta: string;
+  lang: string;
 }
-const Masthead: React.FC<IMasthead> = ({ title, description, cta }) => {
+const Masthead: React.FC<IMasthead> = ({ title, description, cta, lang }) => {
   return (
     <main className={globalStyles.masthead}>
       <div className={globalStyles.container}>
         <div className={styles.masthead__inner}>
           <header className={styles.masthead__content}>
-            <h1 className={styles.masthead__title}>Hi, I'm Andres, Mobile and Web developer.</h1>
-            <Underline />
+            <h1 className={styles.masthead__title}>
+              {title}
+              <br />
+              <DynamicWords lang={lang} />
+            </h1>
             <p className={styles.masthead__description}>{description}</p>
             <a className={styles.masthead__btn} href="#contact">
               {cta}
