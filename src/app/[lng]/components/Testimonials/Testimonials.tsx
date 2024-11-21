23a9593b5047c3ab4af1ed834bf4847ca027b271
Andrés Banda <andrezh_gln7@live.com>
2024-11-21 14:16:14 -0500

    modify testimonials copy and style

--- src/app/[lng]/components/Testimonials/Testimonials.tsx
+++ src/app/[lng]/components/Testimonials/Testimonials.tsx
@@ -3,24 +3,31 @@ import globalStyles from '../../page.module.scss';
 
 const Testimonials = () => {
   return (
-    <main className={styles.contact}>
+    <main className={globalStyles.projects}>
       <div className={globalStyles.container}>
-        <h2 className={globalStyles.section__title}>Testimonials</h2>
-        <div>
-          <div>
-            <img src="" alt="" />
-            <p>Testimonial</p>
-            <p>Author</p>
+        <h2 className={globalStyles.section__title}>¿Qué dicen nuestros clientes?</h2>
+        <div className={styles.testimonials}>
+          <div className={styles.testimonials__item}>
+            <p>
+              Lo que más me impresionó de Abstudio fue su capacidad para comprender nuestro negocio
+              y nuestras necesidades.
+            </p>
+            <p>- María Fernanda Sánchez</p>
           </div>
-          <div>
-            <img src="" alt="" />
-            <p>Testimonial</p>
-            <p>Author</p>
+          <div className={styles.testimonials__item}>
+            <p>
+              Trabajar con Abstudio ha sido una experiencia transformadora para nuestro negocio. Nos
+              han ayudado a crear una presencia online sólida y coherente y a conectar con nuestra
+              audiencia.
+            </p>
+            <p>- Juan Pablo Rodriguez</p>
           </div>
-          <div>
-            <img src="" alt="" />
-            <p>Testimonial</p>
-            <p>Author</p>
+          <div className={styles.testimonials__item}>
+            <p>
+              ¡Recomendamos sus servicios a cualquier empresa que busque un crecimiento real y
+              sostenible!
+            </p>
+            <p>- Ana Lucía Gómez</p>
           </div>
         </div>
       </div>
