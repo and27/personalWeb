851971ecb2147f232a52be8f989ff4016131eb59
Andrés Banda <andrezh_gln7@live.com>
2024-12-31 19:45:35 -0500

    feat: add personal masthead component

--- src/app/[lng]/components/Masthead/Masthead.tsx
+++ src/app/[lng]/components/Masthead/Masthead.tsx
@@ -34,4 +34,23 @@ const Masthead: React.FC<IMasthead> = ({ title, description, cta, lang }) => {
   );
 };
 
+export const PersonalMasthead: React.FC<IMasthead> = ({ title, description, cta, lang }) => {
+  return (
+    <main className={styles.masthead}>
+      <div className={globalStyles.container}>
+        <div className={styles.masthead__inner}>
+          <header className={styles.masthead__content}>
+            <h1 className={styles.masthead__title}>{title}</h1>
+            <p className={styles.masthead__description}>{description}</p>
+            <a className={styles.masthead__btn} href="#contact">
+              {cta}
+            </a>
+          </header>
+          <div className={styles.photo}></div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
 export default Masthead;
