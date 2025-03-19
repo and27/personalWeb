5ed07b48f0e5420fa3064ae7543018e135a5b952
Andrés Banda <andrezh_gln7@live.com>
2025-03-18 22:01:43 -0500

    feat(projects): add stagger animation

--- src/app/[lng]/components/ProjectStack/ProjectStack.tsx
+++ src/app/[lng]/components/ProjectStack/ProjectStack.tsx
@@ -1,13 +1,16 @@
 import ProjectCard from '../Cards/ProjectCard/ProjectCard';
 import globalStyles from '../../page.module.scss';
+import styles from './ProjectStack.module.scss';
 
 const ProjectStack = ({ projects }: { projects: any[] }) => {
   return (
     <section className={globalStyles.blogHome}>
       <div className={globalStyles.container}>
-        {projects.map((project, index) => (
-          <ProjectCard key={index} project={project} index={index} />
-        ))}
+        <div className={styles.projects}>
+          {projects.map((project, index) => (
+            <ProjectCard key={index} project={project} index={index} />
+          ))}
+        </div>
       </div>
     </section>
   );
