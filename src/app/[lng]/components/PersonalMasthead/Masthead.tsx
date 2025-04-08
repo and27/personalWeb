34991991c4887ba23ce108780509f713f4b28eca
Andrés Banda <andrezh_gln7@live.com>
2025-04-08 00:50:11 -0500

    feat: add featured projects gallery

---
+++ src/app/[lng]/components/PersonalMasthead/Masthead.tsx
@@ -0,0 +1,30 @@
+import styles from './Masthead.module.scss';
+import globalStyles from '../../page.module.scss';
+
+interface IMasthead {
+  title: string;
+  description: string;
+  cta: string;
+  lang: string;
+}
+
+const PersonalMasthead: React.FC<IMasthead> = ({ title, description, cta, lang }) => {
+  return (
+    <main className={styles.masthead}>
+      <div className={globalStyles.container}>
+        <div className={styles.masthead__inner}>
+          <header className={styles.masthead__content}>
+            <h1 className={styles.masthead__title}>{title}</h1>
+            <p className={styles.masthead__description}>{description}</p>
+            <a className={styles.masthead__btn} href="#contact">
+              {cta}
+            </a>
+          </header>
+          <div className={styles.photo}></div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default PersonalMasthead;
